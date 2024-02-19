a0cda4b00f11566616aff96cd3a779cde2d8e4c0
Lee-smith-hero <390026008@qq.com>
2024-02-20 00:15:17 +0800

    feat: 轮播图改为swiper组件

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,45 +1,53 @@
-import yay from '@/assets/yay.jpg';
-import { Carousel, Divider, Flex, Typography } from 'antd';
+import banner1 from '@/assets/banner1.jpg';
+import banner3 from '@/assets/banner3.jpg';
+import hzy from '@/assets/hzy.png';
+import { Divider, Flex, Typography } from 'antd';
+import { A11y, Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 import styles from './index.less';
+
 const { Title, Paragraph, Text, Link } = Typography;
 
 export default function HomePage() {
   return (
     <Typography style={{ background: '#fff', fontSize: 16 }}>
-      <Carousel
-        autoplay
-        autoplaySpeed={5000}
-        className={styles.carousel}
-        draggable
-        pauseOnHover={false}
-        pauseOnDotsHover
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        style={{ height: '100vh', top: '-64px' }}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        speed={1000}
       >
-        <div>
-          <img width={'100%'} height={'100%'} src={yay} />
-        </div>
-        <div>
+        <SwiperSlide>
+          <img width={'100%'} src={banner1} />
+        </SwiperSlide>
+        <SwiperSlide>
           <img
-            width={280}
-            height={280}
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            width={'100%'}
+            src={banner3}
+            style={{ objectFit: 'cover', objectPosition: 'top' }}
           />
-        </div>
-        <div>
-          <img width={'100%'} height={'100%'} src={yay} />
-        </div>
-        <div>
-          <img
-            width={280}
-            height={280}
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-          />
-        </div>
-      </Carousel>
+        </SwiperSlide>
+      </Swiper>
       <Flex justify="center" className={styles.homeContainer}>
         <img
+          src={hzy}
           width={280}
           height={280}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          style={{ objectFit: 'cover', objectPosition: 'top' }}
         />
         <Paragraph style={{ width: 770, marginLeft: 30 }}>
           <Paragraph style={{ margin: '10px 0 25px' }}>
@@ -52,11 +60,12 @@ export default function HomePage() {
             >
               韩智勇
             </Title>
+
             <Text style={{ marginLeft: 10, color: '#999', fontSize: 16 }}>
-              研究员 博士生导师 省级人才称号
+              工学博士 地质工程与地质资源博士后 教授 博士生导师
             </Text>
             <Paragraph style={{ color: '#808080', marginTop: 5, fontSize: 16 }}>
-              植物分子毒理与分子生态课题组
+              固体废物管理与资源化处理课题组
             </Paragraph>
           </Paragraph>
           <Paragraph
@@ -67,9 +76,8 @@ export default function HomePage() {
               fontSize: 16,
             }}
           >
-            韩智勇，男，朝鲜族，1980年02月出生于吉林长春。成都理工大学生态环境学院研究员、博士生导师，学术带头人后备人选，省级青年人才项目入选者。生态环境学院综合实验中心主任，四川省科技青年联合会常务理事，四川省植物学会理事，四川省欧美同学会理事，成都理工大学欧美同学会副会长。本科、硕士分别毕业于沈阳农业大学植物保护专业和农药学专业，博士毕业于韩国庆尚国立大学应用生命科学专业。2014年01月至2017年07月于美国德州农工大学完成了博士后研究工作，于2017年07月归国。2018年03月至2018年05月就职于四川农业大学农学院，2018年05月入职成都理工大学生态环境学院。指导本科生和研究生多次获得校级优秀学位论文荣誉。目前，在Developmental
-            Cell, Rice, Plant and Soil, Frontiers in Plant
-            Science等国内外知名学术期刊发表论文40余篇。
+            韩智勇，男，岐佬族，1983年03月出生于贵州。教授，博士生导师，地质灾害防治与地质环境保护国家重点实验室（成都理工大学）、国家环境保护水土污染协同控制与联合修复重点实验室（成都理工大学）固定研究人员；四川省第十二批学术和技术带头人后备人选、四川省高级海外高层次人才；美国特拉华大学访问学者；四川省科技厅、环保厅专家库成员，四川省重点行业土壤调查、四川省固管中心专家库成员，成都市环境保护局环保咨询专家；中国硅酸盐学会固废与生态材料分会危险固废学术委员会常务委员、中国土壤学会青年工作委员会创新委员；环境影响评价工程师、注册环保工程师、清洁生产审核师、环境监理工程师；环境工程国家一流专业负责人，固体废物处理处置与资源化四川省一流课程负责人。
+            主要从事固体废物管理与资源化利用，以及土壤与地下水污染评价与修复等方向的教学和研究工作。系统阐述了我国农村生活垃圾的特性，构建了基于处理技术的全过程管理模式，研发了生物反应器处理技术；系统识别了我国生活垃圾填埋场土壤重金属和地下水污染物，初步探讨了其影响因素。近5年，已先后主持国家自然基金1项，中国博后基金2项，其他省部级项目8项；参加国家重大科技项目2项，出版专著1部，教材1部，参编农业部标准1项；已获授权专利6项，在国内外期刊上已发表论文40余篇。
           </Paragraph>
         </Paragraph>
       </Flex>
@@ -79,15 +87,24 @@ export default function HomePage() {
           <a className={styles.text}>更多 {'>'}</a>
         </Title>
         <Divider style={{ margin: '10px 0 20px' }}></Divider>
-        <div>（一）重金属、新污染物污染土壤植物修复及其机理研究</div>
-        <span style={{ marginLeft: '2em' }}></span>
-        从植物生理、生化、分子生物学与遗传学的角度，研究在重金属镉、抗生素、微塑料等造成污染的土壤中植物对重金属的吸收及转运机理。本研究一方面是了解重金属在作物中的吸收与转运，以为育种及食品安全提供理论基础，另外也为利用植物吸收、转移土壤中的重金属，实现生物修复提供重要的理论支撑。
-        <div>（二）植物中逆境胁迫相关遗传资源筛选与功能研究</div>
-        <span style={{ marginLeft: '2em' }}></span>
-        以作物水稻、黑麦草、微藻等为究对象，对植物中逆境胁迫（水分、盐、极端温度等）相关基因进行筛选，并对其功能及分子机制进行分析。
-        <div>（三）沙化土壤的生物治理技术与机理研究</div>
-        <span style={{ marginLeft: '2em' }}></span>
-        针对川西北地区（若尔盖）草原沙化比较突出的问题，利用草种、微生物、肥料、植物激素、养护技术等恢复沙地植物盖度及区域生态，并对沙化治理过程中抗逆植物基因的功能与应用及植物与土壤微生物的互作机理等开展研究。
+        <div>（一）固体废物管理与资源化处理</div>
+        <span style={{ marginLeft: '2em' }} />
+        （1）农村生活垃圾全过程管理
+        <br />
+        <span style={{ marginLeft: '2em' }} />
+        （2）生物反应器处理技术优化与应用
+        <br />
+        <span style={{ marginLeft: '2em' }} />
+        （3）存量垃圾资源化利用技术研发
+        <div>（二）生活垃圾填埋场土壤与地下水污染评价与修复</div>
+        <span style={{ marginLeft: '2em' }} />
+        （1）填埋场污染场地环境风险评价；
+        <br />
+        <span style={{ marginLeft: '2em' }} />
+        （2）填埋场土壤与地下水污染物识别、归趋与溯源；
+        <br />
+        <span style={{ marginLeft: '2em' }} />
+        （3）填埋场土壤与地下水水土协同污染控制技术
       </div>
 
       <div className={styles.homeContainer}>
@@ -111,6 +128,29 @@ export default function HomePage() {
           友情链接
         </Title>
         <Divider style={{ margin: '10px 0 20px' }}></Divider>
+        <Flex align="center" justify="space-between">
+          <a
+            className={styles.text}
+            target="_blank"
+            href="https://www.cdut.edu.cn/"
+          >
+            成都理工大学
+          </a>
+          <a
+            className={styles.text}
+            target="_blank"
+            href="https://sthjxy.cdut.edu.cn/"
+          >
+            成都理工大学生态环境学院
+          </a>
+          <a
+            className={styles.text}
+            target="_blank"
+            href="https://sthjxy.cdut.edu.cn/info/1105/1660.htm"
+          >
+            成都理工大学生态环境学院——韩智勇
+          </a>
+        </Flex>
       </div>
     </Typography>
   );
