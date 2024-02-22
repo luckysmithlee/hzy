22e49f928f903f16f8584aae4d83efa30f512ad9
Lee-smith-hero <390026008@qq.com>
2024-02-23 01:14:43 +0800

    feat: 首页适配移动端；调整路由

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,5 @@
 import logo from '@/assets/logo.png';
+import { CloseOutlined, MenuOutlined } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
 import { useEffect, useState } from 'react';
 import { Outlet, history } from 'umi';
@@ -7,21 +8,26 @@ const { Header, Content, Footer } = Layout;
 
 const items = [
   { key: 'home', label: '首页' },
-  { key: 'achievements&Papers', label: '研究方向' },
-  { key: 'activity', label: '成果及论文' },
-  { key: 'contact_us', label: '组员介绍' },
-  { key: 'introduction', label: '课题组新闻' },
-  { key: 'join_us', label: '组内活动' },
-  { key: 'news', label: '加入我们' },
-  { key: 'research_direction', label: '联系我们' },
-  { key: 'research_resources', label: '科研资源' },
+  { key: 'research_direction', label: '研究方向' },
+  { key: 'achievements&papers', label: '成果及论文' },
+  { key: 'members', label: '组员介绍' },
+  { key: 'news', label: '课题组新闻' },
+  { key: 'activity', label: '组内活动' },
+  { key: 'contact_us', label: '联系我们' },
+  // { key: 'research_resources', label: '科研资源' },
 ];
 
 export default function indexLayout() {
   const isHome = location.href.includes('/home');
   const [shownav, setShowNav] = useState(!isHome);
+  const [isMobile, setIsMobile] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
+    if (window.screen.width < 500) {
+      // 当前设备是移动设备
+      setIsMobile(true);
+    }
     window.addEventListener('scroll', () => {
       const scrollTop =
         document.documentElement.scrollTop || document.body.scrollTop;
@@ -49,16 +55,19 @@ export default function indexLayout() {
     <Layout>
       <Header
         className={styles.header}
-        style={shownav ? {} : { background: 'transparent' }}
+        style={shownav || isMobile ? {} : { background: 'transparent' }}
       >
-        <div className={styles.logo}>
+        <div className={styles.logo} onClick={() => history.push('home')}>
           <img src={logo} alt="成都理工大学" style={{ height: 35 }} />
-          <span className={styles.hzy}>
+          <span className={styles.hanliu}>
             <i>—— 有温度的韩流</i>
           </span>
         </div>
+        <div className={styles.icon} onClick={() => setShowMenu(!showMenu)}>
+          {isMobile ? showMenu ? <CloseOutlined /> : <MenuOutlined /> : null}
+        </div>
         <Menu
-          mode="horizontal"
+          mode={isMobile ? 'inline' : 'horizontal'}
           theme="dark"
           items={items}
           className={styles.menu}
@@ -74,7 +83,7 @@ export default function indexLayout() {
           <Breadcrumb.Item>List</Breadcrumb.Item>
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb> */}
-        <Outlet />
+        <Outlet context={{ isMobile }} />
       </Content>
       {/* <Footer style={{ textAlign: 'center' }}>
         Ant Design ©{new Date().getFullYear()} Created by Ant UED
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,7 @@ import {
   Parallax,
 } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useOutletContext } from 'umi';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -21,6 +22,8 @@ import styles from './index.less';
 const { Title, Paragraph, Text, Link } = Typography;
 
 export default function HomePage() {
+  const { isMobile = false } = useOutletContext() as any;
+
   const handleTransitionStart = (swiper) => {
     const activeSlide = swiper.slides[swiper.activeIndex];
     activeSlide.classList.add(styles['zoom-out']);
@@ -35,28 +38,24 @@ export default function HomePage() {
     <Typography style={{ background: '#fff', fontSize: 16 }}>
       <Swiper
         modules={[Navigation, Pagination, Autoplay, A11y, Parallax]}
-        spaceBetween={100}
+        // spaceBetween={100}
         slidesPerView={1}
         pagination={{ clickable: true }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}
-        style={{ height: '100vh', top: '-64px' }}
+        // style={{ height: '100vh', top: '-64px' }}
         loop={true}
         autoplay={{
           delay: 5000,
           disableOnInteraction: false,
         }}
         speed={1000}
-        parallax={true}
+        // parallax={true}
         onSlideChangeTransitionStart={handleTransitionStart}
         onSlideChangeTransitionEnd={handleTransitionEnd}
       >
         <SwiperSlide>
-          <img
-            width={'100%'}
-            src={banner1}
-            data-swiper-parallax-scale="1.15"
-          />
+          <img width={'100%'} src={banner1} data-swiper-parallax-scale="1.15" />
         </SwiperSlide>
         <SwiperSlide>
           <img
@@ -70,44 +69,51 @@ export default function HomePage() {
           />
         </SwiperSlide>
       </Swiper>
-      <Flex justify="center" className={styles.homeContainer}>
+      <Flex
+        vertical
+        align="center"
+        justify="center"
+        className={styles.homeContainer}
+      >
         <img
           src={hzy}
-          width={280}
-          height={280}
-          style={{ objectFit: 'cover', objectPosition: 'top' }}
+          width={180}
+          height={180}
+          style={{
+            objectFit: 'cover',
+            objectPosition: 'top',
+            borderRadius: '50%',
+          }}
         />
-        <Paragraph style={{ width: 770, marginLeft: 30 }}>
-          <Paragraph style={{ margin: '10px 0 25px' }}>
-            <Title
-              style={{
-                display: 'inline-block',
-                fontSize: 24,
-                margin: 0,
-              }}
-            >
-              韩智勇
-            </Title>
 
-            <Text style={{ marginLeft: 10, color: '#999', fontSize: 16 }}>
-              工学博士 地质工程与地质资源博士后 教授 博士生导师
-            </Text>
-            <Paragraph style={{ color: '#808080', marginTop: 5, fontSize: 16 }}>
-              固体废物管理与资源化处理课题组
-            </Paragraph>
-          </Paragraph>
-          <Paragraph
-            ellipsis={{ rows: 7 }}
+        <Flex vertical align="center" style={{ margin: '10px 0 25px' }}>
+          <Title
             style={{
-              color: '#808080',
-              lineHeight: '26px',
-              fontSize: 16,
+              display: 'inline-block',
+              fontSize: 24,
+              margin: 0,
             }}
           >
-            韩智勇，男，岐佬族，1983年03月出生于贵州。教授，博士生导师，地质灾害防治与地质环境保护国家重点实验室（成都理工大学）、国家环境保护水土污染协同控制与联合修复重点实验室（成都理工大学）固定研究人员；四川省第十二批学术和技术带头人后备人选、四川省高级海外高层次人才；美国特拉华大学访问学者；四川省科技厅、环保厅专家库成员，四川省重点行业土壤调查、四川省固管中心专家库成员，成都市环境保护局环保咨询专家；中国硅酸盐学会固废与生态材料分会危险固废学术委员会常务委员、中国土壤学会青年工作委员会创新委员；环境影响评价工程师、注册环保工程师、清洁生产审核师、环境监理工程师；环境工程国家一流专业负责人，固体废物处理处置与资源化四川省一流课程负责人。
-            主要从事固体废物管理与资源化利用，以及土壤与地下水污染评价与修复等方向的教学和研究工作。系统阐述了我国农村生活垃圾的特性，构建了基于处理技术的全过程管理模式，研发了生物反应器处理技术；系统识别了我国生活垃圾填埋场土壤重金属和地下水污染物，初步探讨了其影响因素。近5年，已先后主持国家自然基金1项，中国博后基金2项，其他省部级项目8项；参加国家重大科技项目2项，出版专著1部，教材1部，参编农业部标准1项；已获授权专利6项，在国内外期刊上已发表论文40余篇。
-          </Paragraph>
-        </Paragraph>
+            韩智勇
+          </Title>
+          <div style={{ color: '#999', marginTop: 5, fontSize: 16 }}>
+            工学博士 地质工程与地质资源博士后 教授 博士生导师
+          </div>
+          <div style={{ color: '#808080', marginTop: 5, fontSize: 16 }}>
+            固体废物管理与资源化处理课题组
+          </div>
+        </Flex>
+        {/* <Paragraph
+          ellipsis={{ rows: 7 }}
+          style={{
+            color: '#808080',
+            lineHeight: '26px',
+            fontSize: 16,
+          }}
+        >
+          韩智勇，男，岐佬族，1983年03月出生于贵州。教授，博士生导师，地质灾害防治与地质环境保护国家重点实验室（成都理工大学）、国家环境保护水土污染协同控制与联合修复重点实验室（成都理工大学）固定研究人员；四川省第十二批学术和技术带头人后备人选、四川省高级海外高层次人才；美国特拉华大学访问学者；四川省科技厅、环保厅专家库成员，四川省重点行业土壤调查、四川省固管中心专家库成员，成都市环境保护局环保咨询专家；中国硅酸盐学会固废与生态材料分会危险固废学术委员会常务委员、中国土壤学会青年工作委员会创新委员；环境影响评价工程师、注册环保工程师、清洁生产审核师、环境监理工程师；环境工程国家一流专业负责人，固体废物处理处置与资源化四川省一流课程负责人。
+          主要从事固体废物管理与资源化利用，以及土壤与地下水污染评价与修复等方向的教学和研究工作。系统阐述了我国农村生活垃圾的特性，构建了基于处理技术的全过程管理模式，研发了生物反应器处理技术；系统识别了我国生活垃圾填埋场土壤重金属和地下水污染物，初步探讨了其影响因素。近5年，已先后主持国家自然基金1项，中国博后基金2项，其他省部级项目8项；参加国家重大科技项目2项，出版专著1部，教材1部，参编农业部标准1项；已获授权专利6项，在国内外期刊上已发表论文40余篇。
+        </Paragraph> */}
       </Flex>
       <div className={styles.homeContainer}>
         <Title level={4} className={styles.contentTitle}>
@@ -156,7 +162,7 @@ export default function HomePage() {
           友情链接
         </Title>
         <Divider style={{ margin: '10px 0 20px' }}></Divider>
-        <Flex align="center" justify="space-between">
+        <Flex vertical={isMobile} justify="space-between" wrap="wrap">
           <a
             className={styles.text}
             target="_blank"
